9bed3b0dd448c04fddf797b41ee99c37812d0e65
Jac <jac.miller@uleth.ca>
2021-03-24 10:37:36 -0600

    again but with feeling

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ var url = "https://pokeapi.co/api/v2/pokemon/?limit=809";
 
 $("#submit").on("click",function() {
 
-// 	query = "&count=3"
 	callAPI()
 
 function callAPI () {
-// 	var url = url1 + ur12 + url3 + query;
 
 	xhr.open("GET", url, true);
 	xhr.send(null);
@@ -19,15 +17,9 @@ function callAPI () {
 
 			console.log(xhr.responseText);
 			var data = JSON.parse(xhr.responseText);
-			// console.log(data.results.name)
 			var name = data.results[Math.floor((Math.random() * 808)+ 1)].name
 
-			// var ability = data.abilities[0]
-			// var sprite = data.sprites
 			console.log(data)
-			// var name = data[0].url
-
-			// console.log(data.ability)
 
 			$("#one").text(name);
 
@@ -43,21 +35,13 @@ var url2 = "https://pokeapi.co/api/v2/ability/?limit=327"
 	xhr2.onload = function(){
 		if(xhr2.status == 200){
 
-			// ($"#two").empty()
-			// document.write(xhr.responseText)	
 
 			console.log(xhr2.responseText);
 			var data2 = JSON.parse(xhr2.responseText);
-			// console.log(data.results.name)
 			var ability = data2.results[Math.floor((Math.random() * 326)+ 1)].name
 
-			// var ability = data.abilities[0]
-			// var sprite = data.sprites
 			console.log(data2)
-			// var name = data[0].url
 
-			// console.log(data.ability)
-			// $("body").append("<img src="	+ sprite +	">");
 			$("#two").text(ability);
 
 
@@ -73,16 +57,11 @@ var url3 = "https://pokeapi.co/api/v2/nature/?limit=25"
 
 	xhr3.onload = function(){
 		if(xhr3.status == 200){
-			// ($"#three").empty()
-			// document.write(xhr.responseText)	
 
 			console.log(xhr3.responseText);
 			var data3 = JSON.parse(xhr3.responseText);
-			// console.log(data.results.name)
 			var nature = data3.results[Math.floor((Math.random() * 24)+ 1)].name
 
-			// var ability = data.abilities[0]
-			// var sprite = data.sprites
 			console.log(data3)
 
 			$("#three").text(nature);
@@ -91,6 +70,7 @@ var url3 = "https://pokeapi.co/api/v2/nature/?limit=25"
 }
 }
 
+//this code is by user felix
 var randomNum = Math.floor((Math.random()*809)+1);
 $('#my_image').attr('src', 'sprites-master/sprites/pokemon/' + randomNum + '.png');
 
